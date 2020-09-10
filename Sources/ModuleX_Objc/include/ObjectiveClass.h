fb15100c1b9ed63cece2368ecbda6d338586b727
julurik <karthik.juluri@carrier.com>
2020-09-11 03:25:32 +0530

    Create ObjectiveClass.h

---
+++ Sources/ModuleX_Objc/include/ObjectiveClass.h
@@ -0,0 +1,19 @@
+//
+//  ObjectiveClass.h
+//  SampleTestApp
+//
+//  Created by karthik j on 03/09/20.
+//  Copyright © 2020 karthik j. All rights reserved.
+//
+
+
+
+#import <Foundation/Foundation.h>
+
+#if TARGET_OS_MAC
+#import <AppKit/NSView.h>
+@interface ObjectiveClass : NSView
+ @end
+#endif
+
+
