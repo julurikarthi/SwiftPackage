62c4b50800c9a1ca119c06605132bf54016fc49d
julurik <karthik.juluri@carrier.com>
2020-09-11 03:46:41 +0530

    Create TestObj.h
    d3

---
+++ Sources/ModuleX_Objc/include/TestObj.h
@@ -0,0 +1,17 @@
+//
+//  TestObj.h
+//  SampleTestApp
+//
+//  Created by karthik j on 11/09/20.
+//  Copyright © 2020 karthik j. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface TestObj : NSObject
+@property(nonatomic,strong) NSString *name;
+@end
+
+
