b3032f5ccf0a6fc2a9436d4d701088ed29c63a67
julurik <karthik.juluri@carrier.com>
2020-09-11 03:27:40 +0530

    Create Network.swift

---
+++ Sources/MyLibrary/Network.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by karthik j on 04/09/20.
+//
+
+import Foundation
+import SwiftClasses
+class Network {
+    
+
+    func test() {
+//        var cls = SwiftClasses()
+//        var cls = SwiftClasses()
+    }
+}
