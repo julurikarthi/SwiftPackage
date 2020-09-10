6a6da3249da3b3c44be2e12baaa1ccc423b34aca
julurik <karthik.juluri@carrier.com>
2020-09-11 03:26:24 +0530

    Create ObjectiveClass.m

---
+++ Sources/ModuleX_Objc/ObjectiveClass.m
@@ -0,0 +1,16 @@
+//
+//  ObjectiveClass.m
+//  SampleTestApp
+//
+//  Created by karthik j on 03/09/20.
+//  Copyright © 2020 karthik j. All rights reserved.
+//
+
+#import "ObjectiveClass.h"
+
+
+
+@implementation ObjectiveClass
+
+
+@end
