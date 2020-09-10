ffed2f5f8ee031055dc79236ac030e5fef5f0ee6
julurik <karthik.juluri@carrier.com>
2020-09-11 03:46:46 +0530

    Create TestObj.m

---
+++ Sources/ModuleX_Objc/TestObj.m
@@ -0,0 +1,13 @@
+//
+//  TestObj.m
+//  SampleTestApp
+//
+//  Created by karthik j on 11/09/20.
+//  Copyright © 2020 karthik j. All rights reserved.
+//
+
+#import "TestObj.h"
+
+@implementation TestObj
+
+@end
