c2e5587d2f43d37268e107639367a55cf192dced
julurik <karthik.juluri@carrier.com>
2020-09-11 12:02:49 +0530

    Create ObjClass.m

---
+++ Sources/ModuleX_Objc/ObjClass.m
@@ -0,0 +1,18 @@
+//
+//  ObjClass.m
+//  SampleTestApp
+//
+//  Created by karthik j on 11/09/20.
+//  Copyright © 2020 karthik j. All rights reserved.
+//
+
+#import "ObjClass.h"
+
+@implementation ObjClass
+-(void)testIOS {
+    printf("cllaed testios");
+}
+-(void)testmac {
+    printf("cllaed testmac");
+}
+@end
